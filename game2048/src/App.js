3f18217b946c0983967da1c164c5e4a6a5a54d06
CJ <cj>
2021-04-24 17:26:28 +0900

    3챕터 2048 작업 완료

---
+++ game2048/src/App.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import AboveGame from './component/AboveGame';
+import Game from './component/Game';
+import Header from './component/Header';
+import useLocalStorageNumber from './hook/useLocalStorage';
+
+export default function App() {
+  const [score, setScore] = useState(0);
+  const [bestScore, setBestScore] = useLocalStorageNumber('bestScore', 0);
+
+  useEffect(() => {
+    if(score > bestScore) {
+      setBestScore(score);
+    }
+  })
+
+  return  <div className="container">
+    <Header score={score} bestScore={bestScore} />
+    <AboveGame />
+    <Game setScore={setScore} />
+  </div>;
+}
